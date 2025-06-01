bbee45368fd6964b1dba97b25fde742e71a293b5
EC2 Default User <ec2-user@ip-172-31-67-108.ec2.internal>
2025-06-01 18:55:34 +0000

    Updated ALB and EC2 configuration for deployment

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -6,7 +6,7 @@ resource "aws_lb" "app_lb" {
     aws_subnet.subnet_a.id,
     aws_subnet.subnet_b.id,
   ]
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.instance_sg.id]
 }
 
 resource "aws_lb_target_group" "frontend_tg" {
@@ -23,42 +23,56 @@ resource "aws_lb_target_group" "backend_tg" {
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "instance"
+  
+  health_check {
+    path                = "/api/health" 
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
-# resource "aws_lb_listener" "http" {
-#   load_balancer_arn = aws_lb.app_lb.arn
-#   port              = 80
-#   protocol          = "HTTP"
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.frontend_tg.arn
-#   }
-# }
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
 
-# resource "aws_lb_listener_rule" "backend_rule" {
-#   listener_arn = aws_lb_listener.http.arn
-#   priority     = 10
-#   action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.backend_tg.arn
-#   }
-#   condition {
-#     path_pattern {
-#       values = ["/api/*"]
-#     }
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.frontend_tg.arn
+  }
+}
 
-# resource "aws_lb_target_group_attachment" "frontend_attach" {
-#   target_group_arn = aws_lb_target_group.frontend_tg.arn
-#   target_id        = aws_instance.app.id
-#   port             = 3000
-# }
+resource "aws_lb_listener_rule" "backend_rule" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 10
 
-# resource "aws_lb_target_group_attachment" "backend_attach" {
-#   target_group_arn = aws_lb_target_group.backend_tg.arn
-#   target_id        = aws_instance.app.id
-#   port             = 5000
-# }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend_tg.arn
+  }
 
+  condition {
+    path_pattern {
+      values = ["/api/*"]
+    }
+  }
+
+  depends_on = [
+    aws_lb_listener.http,
+  ]
+}
+resource "aws_lb_target_group_attachment" "backend_attachment" {
+  target_group_arn = aws_lb_target_group.backend_tg.arn
+  target_id        = aws_instance.app.id
+  port             = 5000
+}
+
+resource "aws_lb_target_group_attachment" "frontend_attachment" {
+  target_group_arn = aws_lb_target_group.frontend_tg.arn
+  target_id        = aws_instance.app.id
+  port             = 3000
+}
 
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,18 +1,26 @@
 resource "aws_instance" "app" {
-  ami                         = "ami-0c94855ba95c71c99"
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  subnet_id                   = aws_subnet.subnet_a.id
-  vpc_security_group_ids      = [aws_security_group.instance_sg.id]
+  ami                    = data.aws_ami.amzn2.id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.subnet_a.id
+  security_groups        = [aws_security_group.instance_sg.id]
+  key_name               = "vockey"
   associate_public_ip_address = true
-  user_data                   = file("${path.module}/user_data.sh")
 
-  root_block_device {
-    volume_size = 8
-  }
+  user_data = file("${path.module}/user_data.sh")
 
   tags = {
-    Name = "${var.project_name}-ec2"
+    Name = "${var.project_name}-instance"
+  }
+}
+
+
+data "aws_ami" "amzn2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
 
